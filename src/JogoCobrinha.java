6eba6cdb1303456067e2cbf4a2b5935a33745b3a
Jonathas <funguinha82@gmail.com>
2026-04-10 21:17:44 -0300

    Feat: quando o jogador apertar a tecla barra de espaço o Jogo é iniciado

--- src/JogoCobrinha.java
+++ src/JogoCobrinha.java
@@ -1,16 +1,17 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
-public class JogoCobrinha extends JPanel {
-
-
+public class JogoCobrinha extends JPanel implements ActionListener {
 
 
         private final int width;
         private final int height;
-
+        private static final int FRAME_RATE = 20;
+        private boolean inicioDoJogo = false;
 
     public JogoCobrinha (final int width, final int height){
             super(); // esta herdando do Jpanel
@@ -31,19 +32,30 @@ public class JogoCobrinha extends JPanel {
             public void keyPressed(KeyEvent e) {
 
                if (e.getKeyCode() == KeyEvent.VK_SPACE){
-                   System.out.println("vc apertou espaço");
+                   inicioDoJogo = true;
                }
             }
         }); // mapeeaia o teclado do jogador
 
-        repaint(); // chama o Java swing q abstraem o window tool, biblioteca antiga
+        new Timer(1000/ FRAME_RATE, this).start();
+        //repaint(); // chama o Java swing q abstraem o window tool, biblioteca antiga
     }
 
     @Override
     protected void paintComponent(Graphics g) {
-        super.paintComponent(g); // incia os objetos graficos do java
-        g.setColor(Color.WHITE);
-        g.setFont(g.getFont().deriveFont(30F));
-        g.drawString("COBRINHA",400,200); // escreve uma mensagem como elemento grafico
+        super.paintComponent(g);// incia os objetos graficos do java
+
+        if (!inicioDoJogo) {
+
+
+            g.setColor(Color.WHITE);
+            g.setFont(g.getFont().deriveFont(30F));
+            g.drawString("COBRINHA", 400, 200); // escreve uma mensagem como elemento grafico
+        }
+    }
+
+    @Override
+    public void actionPerformed(final ActionEvent e) {
+        repaint();
     }
 }
