98a86dc1ddc59910c8e976e80d2791c6c6d5eb9e
Jonathas <funguinha82@gmail.com>
2026-04-10 23:05:54 -0300

    controles adicionados

--- src/JogoCobrinha.java
+++ src/JogoCobrinha.java
@@ -18,6 +18,11 @@ public class JogoCobrinha extends JPanel implements ActionListener {
         private final List<GamePoints> cobra = new ArrayList<>();
 
         private final int TamanhoDaCelula;
+        private boolean gameOver = false;
+
+        private Direction direction = Direction.RIGHT;
+        private Direction newDirection = Direction.RIGHT;
+
 
 
     public JogoCobrinha (final int width, final int height){
@@ -40,14 +45,40 @@ public class JogoCobrinha extends JPanel implements ActionListener {
             @Override
             public void keyPressed(KeyEvent e) {
 
-               if (e.getKeyCode() == KeyEvent.VK_SPACE){
-                   inicioDoJogo = true;
-               }
+                handleKey(e.getKeyCode());
             }
         }); // mapeeaia o teclado do jogador
 
         new Timer(1000/ FRAME_RATE, this).start();
         //repaint(); // chama o Java swing q abstraem o window tool, biblioteca antiga
+    }
+
+    private void handleKey(final int keyCode) {
+
+        if (!inicioDoJogo) {
+            if (keyCode == KeyEvent.VK_SPACE) {
+                inicioDoJogo = true;
+            }
+        } else if (!gameOver) {
+            switch (keyCode){
+                case KeyEvent.VK_W:
+                    newDirection = Direction.UP;
+                    break;
+
+                case KeyEvent.VK_S:
+                    newDirection = Direction.DOWN;
+                    break;
+
+                case KeyEvent.VK_D:
+                    newDirection = Direction.RIGHT;
+                    break;
+
+                case KeyEvent.VK_A:
+                    newDirection = Direction.LEFT;
+                    break;
+            }
+
+        }
     }
 
     private void resetGameData(){
@@ -73,13 +104,55 @@ public class JogoCobrinha extends JPanel implements ActionListener {
         }
     }
 
+
+    private void move(){
+        final GamePoints posiçaoDaCabeçaI = cobra.getFirst();
+        direction = newDirection;
+
+        final GamePoints novaCabeca = switch (direction){
+
+            case UP -> new GamePoints(posiçaoDaCabeçaI.x,posiçaoDaCabeçaI.y - TamanhoDaCelula);
+            case DOWN -> new GamePoints(posiçaoDaCabeçaI.x,posiçaoDaCabeçaI.y +TamanhoDaCelula);
+            case LEFT -> new GamePoints(posiçaoDaCabeçaI.x - TamanhoDaCelula,posiçaoDaCabeçaI.y );
+            case RIGHT -> new GamePoints(posiçaoDaCabeçaI.x + TamanhoDaCelula,posiçaoDaCabeçaI.y );
+        };
+        cobra.addFirst(novaCabeca);
+
+
+        if (isCollision()){
+            gameOver = true;
+            cobra.removeFirst();
+        }else {
+            cobra.removeLast();
+        }
+
+        direction = newDirection;
+    }
+
+    private boolean isCollision(){
+        final GamePoints cabeca = cobra.getFirst();
+        final var invalidWidth = (cabeca.x <0)||(cabeca.x >= width );
+        final var invalidHeight = (cabeca.y < 0)||(cabeca.y>=height);
+        if (invalidWidth || invalidHeight){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
     @Override
     public void actionPerformed(final ActionEvent e) {
 
-
+        if (inicioDoJogo && !gameOver){
+            move();
+        }
         repaint();
     }
 
 
     private record GamePoints(int x, int y){}
+
+    private enum Direction{
+        UP, DOWN, RIGHT,LEFT
+    }
 }
